45fc0741c4929b22c37d1ef7e0c76996719e278d
nehirpasali <pasalinehir@gmail.com>
2025-06-11 13:12:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -75,5 +75,17 @@
     </iframe>
     <p>Made by Nehir PAŞALI</p>
   </footer>
+        const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function showSlide() {
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[index].classList.add('active');
+  index = (index + 1) % slides.length;
+}
+
+showSlide();
+setInterval(showSlide, 3000);
+
 </body>
 </html>
